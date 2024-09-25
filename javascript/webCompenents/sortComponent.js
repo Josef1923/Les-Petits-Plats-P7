31b0c5d059b6d5fc63372608ec5f784213c42269
Josef1923 <schererjos01@gmail.com>
2024-09-25 11:44:48 +0200

    Supression HTML pour ajout compenent dynamiquement

--- javascript/webCompenents/sortComponent.js
+++ javascript/webCompenents/sortComponent.js
@@ -2,27 +2,34 @@ class SortComponent extends HTMLElement {
     constructor() {
         super();
 
-        // Créer les éléments (boutons)
+        // Créer le conteneur des boutons
         const container = document.createElement('div');
         container.classList.add('sort-container');
 
+        // Bouton pour Ingrédients
         const sortIngredientsButton = document.createElement('button');
-        sortIngredientsButton.textContent = 'Ingrédients';
+        sortIngredientsButton.innerHTML = 'Ingrédients <img src="assets/icons/vector1.svg">';
         sortIngredientsButton.id = 'sort-ingredients';
 
-        const sortUstensilsButton = document.createElement('button');
-        sortUstensilsButton.textContent = 'Appareils';
-        sortUstensilsButton.id = 'sort-appliance';
-
+        // Bouton pour Appareils
         const sortApplianceButton = document.createElement('button');
-        sortApplianceButton.textContent = 'Ustensiles';
-        sortApplianceButton.id = 'sort-ustensils';
+        sortApplianceButton.innerHTML = 'Appareils <img src="assets/icons/vector1.svg">';
+        sortApplianceButton.id = 'sort-appliance';
 
+        // Bouton pour Ustensiles
+        const sortUstensilsButton = document.createElement('button');
+        sortUstensilsButton.innerHTML = 'Ustensiles <img src="assets/icons/vector1.svg">';
+        sortUstensilsButton.id = 'sort-ustensils';
+
+        // Ajouter les boutons dans le conteneur
         container.appendChild(sortIngredientsButton);
         container.appendChild(sortApplianceButton);
         container.appendChild(sortUstensilsButton);
 
+        // Ajouter le conteneur au Shadow DOM ou DOM du composant
         this.appendChild(container);
-
     }
 }
+
+// Déclarer le Web Component
+customElements.define('sort-component', SortComponent);
