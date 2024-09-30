a1eadf8c1cd7e91ad4f4e6e1548e282ecb28d21a
Josef1923 <schererjos01@gmail.com>
2024-09-30 18:32:38 +0200

    Ajout temps sur les cards

--- javascript/webComponents/cardComponent.js
+++ javascript/webComponents/cardComponent.js
@@ -26,19 +26,22 @@ class Card {
 
         // Retourne le HTML de la carte
         return `
-        <div class="card">
-            <img src="${this.image}" class="card-img-top" alt="${this.name}">
-            <div class="card-body">
-                <h2 class="card-title">${this.name}</h2>
-                <h3 class="recipe-titles">RECETTE</h3>
-                <p class="card-text">${this.description}</p>
-                <h3 class="ingredient-title">INGRÉDIENTS</h3>
-                <div class="ingredients">
-                    ${ingredientsHTML}
-                </div>
+    <div class="card">
+        <div class="recipe-time">
+            <span>${this.time} min</span>
+        </div>
+        <img src="${this.image}" class="card-img-top" alt="${this.name}">
+        <div class="card-body">
+            <h2 class="card-title">${this.name}</h2>
+            <h3 class="recipe-titles">RECETTE</h3>
+            <p class="card-text">${this.description}</p>
+            <h3 class="ingredient-title">INGRÉDIENTS</h3>
+            <div class="ingredients">
+                ${ingredientsHTML}
             </div>
         </div>
-        `;
+    </div>
+    `;
     }
 }
 
